07f1a5af400a5ccec0fa8d5337da1403a91ecf25
Oguzhan Aslan <106531243+OFAslan@users.noreply.github.com>
2024-04-01 23:24:30 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,8 +30,8 @@ product_caption = 'Our warm, comfortable, ' + color_option + ' sweatsuit!'
 
 # use the option selected to go back and get all the info from the database
 
-my_cur.execute("select direct_url, price, size_list, upsell_product_desc from catalog_for_website where
-color_or_style = '" + color_option + "';")
+my_cur.execute("""select direct_url, price, size_list, upsell_product_desc from catalog_for_website where
+color_or_style = '" + color_option + "';""")
 df2 = my_cur.fetchone()
 st.image(
 df2[0],
