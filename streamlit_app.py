3a28b1cdbf4b7c12a7abc12ab134bd9897c509ad
Oguzhan Aslan <106531243+OFAslan@users.noreply.github.com>
2024-04-01 23:24:12 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,5 +28,16 @@ color_option = st.selectbox('Pick a sweatsuit color or style:', list(color_list)
 
 product_caption = 'Our warm, comfortable, ' + color_option + ' sweatsuit!'
 
-st.write(product_caption)
+# use the option selected to go back and get all the info from the database
 
+my_cur.execute("select direct_url, price, size_list, upsell_product_desc from catalog_for_website where
+color_or_style = '" + color_option + "';")
+df2 = my_cur.fetchone()
+st.image(
+df2[0],
+width=400,
+caption= product_caption
+)
+st.write('Price: ', df2[1])
+st.write('Sizes Available: ',df2[2])
+st.write(df2[3])
