6c18e8f889fe67130a43f06d5809f1d68574064e
Oguzhan Aslan <106531243+OFAslan@users.noreply.github.com>
2024-04-01 23:48:48 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,12 @@ color_option = st.selectbox('Pick a sweatsuit color or style:', list(pd_df.loc[:
 #let's create a image caption and use it for the image of each product
 product_caption = 'Our warm, comfortable, ' + color_option + ' sweatsuit!'
 
-st.image(pd_df.loc[pd_df['COLOR_OR_STYLE'] == color_option, 'DIRECT_URL'].iloc[0], caption=product_caption, use_column_width=True)
+st.image(pd_df.loc[pd_df['COLOR_OR_STYLE'] == color_option, 'DIRECT_URL'].iloc[0], caption=product_caption, width = 400)
 
+#now let's write the other details
+
+st.write('Price: ', pd_df.loc[pd_df['COLOR_OR_STYLE'] == color_option, 'PRICE'].iloc[0])
+st.write('Sizes Available: ',pd_df.loc[pd_df['COLOR_OR_STYLE'] == color_option, 'SIZE_LIST'].iloc[0])
+st.write(pd_df.loc[pd_df['COLOR_OR_STYLE'] == color_option, 'UPSELL_PRODUCT_DESC'].iloc[0])
 
 
