bff80d2c062e661c3a8e2a01891d0dba392aebd4
Oguzhan Aslan <106531243+OFAslan@users.noreply.github.com>
2024-04-01 23:25:29 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,7 @@ my_cur.execute("""select direct_url, price, size_list, upsell_product_desc from 
 color_or_style = '" + color_option + "';""")
 df2 = my_cur.fetchone()
 st.image(
-df2[0],
+df2.iloc[0, 0],
 width=400,
 caption= product_caption
 )
